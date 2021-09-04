ac057232e13940fabacf885dbd820e9a770085f7
midorlo <dorloechter@gmail.com>
2021-09-04 17:41:43 +0200

    chore: improve efficiency of AuditorAwareImpl

--- src/main/java/com/midorlo/k9/components/security/AuditorAwareImpl.java
+++ src/main/java/com/midorlo/k9/components/security/AuditorAwareImpl.java
@@ -1,10 +1,11 @@
 package com.midorlo.k9.components.security;
 
 import com.midorlo.k9.domain.security.Account;
-import com.midorlo.k9.service.security.AccountsService;
+import com.midorlo.k9.model.projection.security.AuditorAwareUserDetails;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -18,17 +19,9 @@ public class AuditorAwareImpl implements AuditorAware<Account> {
 
     public static final String AUDITOR_AWARE_IMPL_INSTANCE_NAME = "auditorAwareImpl";
 
-    private final AccountsService accountsService;
-
-    public AuditorAwareImpl(AccountsService accountsService) {
-        this.accountsService = accountsService;
-    }
-
-
     @Override
     @NonNull
     public Optional<Account> getCurrentAuditor() {
-        String currentAuditorAccountname = accountsService.getPrincipal().orElse("System@localhost");
-        return accountsService.findAccountByEmail(currentAuditorAccountname);
+        return Optional.of(((AuditorAwareUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getAccount());
     }
 }
