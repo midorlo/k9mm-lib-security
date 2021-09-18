4c82678037c2da60d91e45d626c98c6648410035
midorlo <dorloechter@gmail.com>
2021-09-19 01:03:36 +0200

    extend defaults

--- src/main/java/com/midorlo/k9/service/security/SecurityUtilities.java
+++ src/main/java/com/midorlo/k9/service/security/SecurityUtilities.java
@@ -1,4 +1,4 @@
-package com.midorlo.k9.service;
+package com.midorlo.k9.service.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
